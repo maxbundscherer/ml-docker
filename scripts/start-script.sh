607de047b2a732daf59d082038ced0d81eeae6ec
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-08-03 19:38:19 +0200

    init

--- scripts/start-script.sh
+++ scripts/start-script.sh
@@ -4,17 +4,17 @@ if [ "$1" = "" ]; then
 fi
 
 if [ "$2" = "" ]; then
-    echo "Error: Please script path"
+    echo "Error: Please enter script path"
     exit
 fi
 
 if [ "$3" = "" ]; then
-    echo "Error: Please script file"
+    echo "Error: Please enter script file"
     exit
 fi
 
-docker run --rm -p 50888:8888 --name ml-bundscherer \
-    -e JUPYTER_ENABLE_LAB=yes -v $1:/home/jovyan/work \
+docker run --rm --name ml-bundscherer \
+    -v $1:/home/jovyan/work \
     image-ml-bundscherer:latest \
     /bin/bash -c "cd work/$2; python $3" >> log.txt
 
