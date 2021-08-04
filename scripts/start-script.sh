3f4a5835cc5e1173bccd3334a76bddcfeb010a0e
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-08-04 11:10:05 +0200

    init

--- scripts/start-script.sh
+++ scripts/start-script.sh
@@ -16,6 +16,6 @@ fi
 docker run --rm --name ml-bundscherer \
     -v $1:/home/jovyan/work \
     image-ml-bundscherer:latest \
-    /bin/bash -c "cd work/$2; python $3" >&2 log.txt
+    /bin/bash -c "cd work/$2; python $3" &> log.txt
 
 
