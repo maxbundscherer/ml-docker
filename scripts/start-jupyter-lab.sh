fa5a697fedfbcf86185783f46734c1fb015435df
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-08-26 14:50:29 +0200

    Improve NB ID

--- scripts/start-jupyter-lab.sh
+++ scripts/start-jupyter-lab.sh
@@ -3,7 +3,12 @@ if [ "$1" = "" ]; then
     exit
 fi
 
+user_id=`id -u`
+echo "- Set NB_UID to $user_id"
+
 docker run --rm -p 50888:8888 --name ml-bundscherer \
     -e JUPYTER_ENABLE_LAB=yes -v $1:/home/jovyan/work \
+    -e NB_UID=$user_id \
+    --user root \
     image-ml-bundscherer:latest \
     start-notebook.sh --NotebookApp.token='abc'
--- scripts/start-script.sh
+++ scripts/start-script.sh
@@ -13,9 +13,12 @@ if [ "$3" = "" ]; then
     exit
 fi
 
+user_id=`id -u`
+echo "- Set NB_UID to $user_id"
+
 docker run --rm --name ml-bundscherer \
     -v $1:/home/jovyan/work \
+    -e NB_UID=$user_id \
+    --user root \
     image-ml-bundscherer:latest \
     /bin/bash -c "cd work/$2; python $3" > log.txt 2>&1
-
-
