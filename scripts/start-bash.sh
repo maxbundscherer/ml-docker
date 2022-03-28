c93c4b48f3d33e241fd4b8947dd0f8d37f1790eb
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-03-28 13:04:04 +0200

    add new dependencies

--- scripts/start-bash.sh
+++ scripts/start-bash.sh
@@ -1,10 +1,4 @@
-if [ "$1" = "" ]; then
-    echo "Error: Please enter path"
-    exit
-fi
-
 docker exec \
         -e NB_UID=$user_id \
-        -v $1:/home/jovyan/work \
         image-ml-bundscherer:latest \
         -it /bin/bash
