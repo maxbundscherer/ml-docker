5f19c451d19ab77db65d6da4b2afe46c1ccbeee4
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-03-28 13:06:17 +0200

    add new dependencies

--- scripts/start-bash.sh
+++ scripts/start-bash.sh
@@ -1,4 +1,36 @@
-docker exec \
+# Start Jupter Lab in Docker Container
+# $1 = path to data-dir
+
+if [ "$1" = "" ]; then
+    echo "Error: Please enter path"
+    exit
+fi
+
+user_id=`id -u`
+echo "- Set NB_UID to $user_id"
+echo "- Mount path $1"
+
+# Set gpus / Remove NV_GPU=... if you want to use only cpu
+# Use nvidia-docker instead of docker if you want tu use gpu
+#NV_GPU=3,4 nvidia-
+docker run --rm --name ml-bundscherer \
+	-d
         -e NB_UID=$user_id \
+        -p 50888:8888 \
+        -v $1:/home/jovyan/work \
         image-ml-bundscherer:latest \
-        -it /bin/bash
+        jupyter lab \
+        --allow-root \
+        --ip 0.0.0.0 \
+        --notebook-dir="/home/jovyan/work" \
+        --NotebookApp.token='abc'
+
+
+docker exec ml-bundscherer -it /bin/bash
+docker stop ml-bundscherer
+
+echo "Docker stoped"
+
+# Deprecated, but may useful:
+#    -e JUPYTER_ENABLE_LAB=yes \
+#    --user root \
